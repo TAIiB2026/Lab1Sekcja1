5cef02a13255fad1d5d29d600573b15c15867ccd
Student RM4-3073 01 <studentlab1401@it.polsl.pl>
2026-03-26 14:36:38 +0100

    zajecia

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,18 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Secondary } from './secondary/secondary';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MyDirectove } from './my-directove';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-  ],
-  bootstrap: [App]
+  declarations: [App, Secondary, MyDirectove],
+  imports: [BrowserModule, CommonModule, AppRoutingModule, FormsModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
